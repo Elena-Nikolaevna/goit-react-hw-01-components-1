f6ff4cfb7d5f2de20e2ed9e9e2195e2e1d467aa4
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-09-25 20:13:54 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user';
+
+
 export const App = () => {
   return (
     <div
-      style={{
+    /*   style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
-      }}
+        color: '#010101',
+      }} */
     >
-      React homework template
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        likes={user.stats.likes}
+        views={user.stats.views}
+        followers={user.stats.followers}
+        
+      />
     </div>
   );
 };
