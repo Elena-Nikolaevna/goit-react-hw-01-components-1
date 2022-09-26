b4271e36bc7415f1bf049af63b4b9d51ad316cfb
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-09-26 21:32:09 +0300

    Update TransactionList.jsx

--- src/components/TransactionHistory/TransactionList.jsx
+++ src/components/TransactionHistory/TransactionList.jsx
@@ -1,6 +1,43 @@
-import { Transaction } from '../TransactionHistory/Transaction';
+//import { Transaction } from '../TransactionHistory/Transaction';
+
+import css from './Transaction.module.css';
+import PropTypes from 'prop-types';
 
 export const TransactionList = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.thead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody className={css.tbody}>
+        {items.map(item => (
+          <tr className={css.tr} key={item.id}>
+            <td className={css.td}>{item.type}</td>
+            <td className={css.td}>{item.amount}</td>
+            <td className={css.td}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+TransactionList.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            type: PropTypes.string,
+            amount: PropTypes.string,
+            currency: PropTypes.string,
+        }).isRequired,
+    ),
+};
+
+/* export const TransactionList = ({ items }) => {
   return (
     <div>
       {items.map((item, idx) => (
@@ -9,3 +46,4 @@ export const TransactionList = ({ items }) => {
     </div>
   );
 };
+ */
